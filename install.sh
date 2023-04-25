afa4754e4077dfa88da32a5098871301525d21b8
Omar Caja García <omar.caja@hotmail.com>
2023-04-25 22:56:22 +0200

    [OC] Updating install.sh file

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cat starship.toml > $HOME/.config/starship.toml
-cat .vimrc >> $HOME/.vimrc
-cat .zshrc >> $HOME/.zshrc
+cat starship/starship.toml > $HOME/.config/starship.toml
+mkdir -p $HOME/.warp/workflows
+cp -a warp/. $HOME/.warp/workflows
+cat vim/.vimrc >> $HOME/.vimrc
+cat zsh/.zshrc >> $HOME/.zshrc
 
