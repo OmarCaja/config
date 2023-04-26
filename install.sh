e2286b7b29714f1e863d39c70d1874ed1bd81e60
Omar Caja García <omar.caja@hotmail.com>
2023-04-26 17:59:51 +0200

    [OC] Refator install.sh and alias workflow

--- install.sh
+++ install.sh
@@ -5,7 +5,15 @@ mkdir -p $HOME/.warp/workflows
 
 cp -rf starship/starship.toml $HOME/.config/starship.toml
 cp -rf warp/workflows/. $HOME/.warp/workflows
-cat vim/.vimrc >> $HOME/.vimrc
-cat zsh/.zshrc >> $HOME/.zshrc
-cat zsh/.aliases >> $HOME/.aliases
+
+concat_file_lines () 
+{
+	while read line; do
+		grep -qF "$line" $2 || echo $line >> $2
+	done < $1
+}
+
+concat_file_lines "vim/.vimrc" "$HOME/.vimrc"
+concat_file_lines "zsh/.zshrc" "$HOME/.zshrc"
+concat_file_lines "zsh/.aliases" "$HOME/.aliases"
 
