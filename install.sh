b13de029fd055147f52091e9bd6d97d60e898501
Omar Caja García <omar.caja@hotmail.com>
2023-04-27 10:37:29 +0200

    Added git config

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+currentDir=$(pwd)
+goconfAlias="alias goconf='cd $currentDir'"
+
 mkdir -p $HOME/.config
 mkdir -p $HOME/.warp/workflows
 
@@ -8,6 +11,7 @@ cp -rf warp/workflows/. $HOME/.warp/workflows
 
 concat_file_lines () 
 {
+	IFS=''
 	while read line; do
 		grep -qF "$line" $2 || echo $line >> $2
 	done < $1
@@ -16,4 +20,7 @@ concat_file_lines ()
 concat_file_lines "vim/.vimrc" "$HOME/.vimrc"
 concat_file_lines "zsh/.zshrc" "$HOME/.zshrc"
 concat_file_lines "zsh/.aliases" "$HOME/.aliases"
+concat_file_lines "git/.gitconfig" "$HOME/.gitconfig"
+
+grep -qF $goconfAlias "$HOME/.aliases" || echo $goconfAlias >> "$HOME/.aliases"
 
