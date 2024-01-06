a3c06c51c6d9ebc4eb4c7f6317ac3d53986b26c2
Omar Caja García <omar.caja@hotmail.com>
2024-01-06 01:23:08 +0100

    Updating install.sh for using gum

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # :rocket: Custom config files for spedding up.
 
+## :info: Prerequisites:
+
+[Homebrew](https://brew.sh/)
+
+[Bash](https://formulae.brew.sh/formula/bash)
+
+`brew install bash`
+
+[Gum](https://github.com/charmbracelet/gum)
+
+`brew install gum`
+
 ## :wrench: Installation.
-Execute `./install.sh` for auto installing the given configuration files:
 
-- `aliases`
-  - `bat` -> Will add `bat` alias for `cat`. See https://github.com/sharkdp/bat.
-  - `exa` -> Will add some `exa` aliases for `ls`. See https://the.exa.website.
-  - `nvim` -> Will add `nvim` alias for `vim`, and will setup `nvim` as default text editor for git.
-  - `zsh` -> Will add some usefull sh aliases.
-- `git` -> Will add some `git` aliases.
-- `starship` -> Will replace your current `starship.toml` config file located in: `$HOME/.config/starship.toml` if exists, otherwise it will create the file. See https://starship.rs/config
-- `warp` -> Will add warp workflows to your warp workflows folder `$HOME/.war/workflows` if the folder exists, otherwise it will create the folder. See https://docs.warp.dev/features/entry/workflows
-
-:grey_exclamation: Check `install.zsh -h` for help
+Execute `./install.sh` for installing the given apps and configuration files:
--- install.sh
+++ install.sh
@@ -1,45 +1,109 @@
-#!/usr/local/Cellar/bash/5.2.15/bin/bash
+#!/usr/local/Cellar/bash/5.2.21/bin/bash
 
-declare -A APPS_NAME_URL=(
-	[brew]=$(gum style --foreground "$PINK" "brew")
+PINK="13"
+CURRENT_DIR=$(pwd)
+
+declare -A APPS_NAME=(
 	[bat]=$(gum style --foreground "$PINK" "bat")
 	[exa]=$(gum style --foreground "$PINK" "exa")
 	[nvim]=$(gum style --foreground "$PINK" "nvim")
 	[starship]=$(gum style --foreground "$PINK" "starship")
+	[git]=$(gum style --foreground "$PINK" "git")
 )
+declare -a ALIASES=("bat" "exa" "nvim" "git")
+declare -a GIT_CONFIG=("nvim" "git")
 
-install_app() {
+install_sh_aliases() {
+
+	concatenate_lines_in_file "# sh aliases" "$HOME/.zshrc"
+	concatenate_lines_in_file "[[ -s \"$CURRENT_DIR/aliases/sh/.aliases\" ]] && source \"$CURRENT_DIR/aliases/sh/.aliases\"" "$HOME/.zshrc"
+}
+
+install_starship() {
+
+	concatenate_lines_in_file "eval \"$(starship init zsh)\"" "$HOME/.zshrc"
+	cp -rf "starship/starship.toml" "$HOME/.config/starship.toml"
+}
+
+install_gitconfig() {
+
+	if ! grep -qF "$1" "$HOME/.gitconfig"; then
+		echo "[include]" >>"$HOME/.gitconfig"
+		echo "$1" >>"$HOME/.gitconfig"
+	fi
+}
+
+concatenate_lines_in_file() {
+
+	grep -qF "$1" "$2" || echo "$1" >>"$2"
+}
+
+install_aliases() {
+
+	concatenate_lines_in_file "# $1 aliases" "$HOME/.zshrc"
+	concatenate_lines_in_file "[[ -s \"$CURRENT_DIR/aliases/$1/.aliases\" ]] && source \"$CURRENT_DIR/aliases/$1/.aliases\"" "$HOME/.zshrc"
+
+	if [[ $1 = nvim ]]; then
+		install_gitconfig "path = $CURRENT_DIR/git/nvim/.gitconfig"
+	fi
+}
+
+brew_install_app() {
 
 	if ! command -v "$1" >/dev/null 2>&1; then
 
-		echo "${APPS_NAME_URL[$1]} is not installed"
+		echo "${APPS_NAME[$1]} is not installed."
 		echo
-		gum spin -s dot --title "Installing ${APPS_NAME_URL[$1]}" sleep 2
+		gum spin -s dot --show-output --title "Installing ${APPS_NAME[$1]} " brew install "$1"
 		echo
-		echo "${APPS_NAME_URL[$1]} installed successfully"
+		echo "${APPS_NAME[$1]} installed successfully"
 		echo
-		
+
 	else
 
-		echo "${APPS_NAME_URL[$1]} is currently installed"
+		echo "${APPS_NAME[$1]} is currently installed"
 		echo
 	fi
 }
 
-install_extra_apps() {
+install_apps() {
 
-	echo Choose other apps you want to install:
+	echo Choose other apps you want to install.
+	echo Press space to select between options and press enter to confirm.
 	echo
-	echo Press space to select one or more options and enter to confirm
-	echo
 
-	apps=$(gum choose --no-limit ${APPS_NAME_URL[@]:2})
+	apps=$(gum choose --no-limit "${APPS_NAME[@]}")
 
+	# shellcheck disable=SC2068
 	for app in ${apps[@]}; do
-		install_app $app
+		brew_install_app "$app"
+		if [ "$app" = starship ]; then
+			install_starship
+		else
+			for alias in "${ALIASES[@]}"; do
+				if [ "$app" = "$alias" ]; then
+					install_aliases "$app"
+				fi
+			done
+			for gitConfig in "${GIT_CONFIG[@]}"; do
+				if [ "$app" = "$gitConfig" ]; then
+					if [ "$app" = nvim ]; then
+						install_gitconfig "path = $CURRENT_DIR/git/nvim/.gitconfig"
+
+					else
+						install_gitconfig "path = $CURRENT_DIR/git/.gitconfig"
+					fi
+				fi
+			done
+		fi
 	done
 }
 
-install_app brew
-install_extra_apps
- 
+touch "$HOME/.zshrc"
+touch "$HOME/.gitconfig"
+mkdir -p "$HOME/.config"
+
+install_apps
+
+gum pager <aliases/sh/.aliases
+gum confirm "Do you want to install the previous aliases?" && install_sh_aliases || echo "Custom aliases not installed"
