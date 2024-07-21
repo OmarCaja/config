70da044eb00b955eee3b9c7637b9385191afa877
Omar Caja García <omar.caja@kaizencode.es>
2024-07-21 20:19:35 +0200

    Changing exa to eza and adding Zed config

--- install.sh
+++ install.sh
@@ -1,109 +1,99 @@
-#!/usr/local/Cellar/bash/5.2.21/bin/bash
+#!/usr/local/Cellar/bash/5.2.26/bin/bash
 
 PINK="13"
 CURRENT_DIR=$(pwd)
 
 declare -A APPS_NAME=(
-	[bat]=$(gum style --foreground "$PINK" "bat")
-	[exa]=$(gum style --foreground "$PINK" "exa")
-	[nvim]=$(gum style --foreground "$PINK" "nvim")
-	[starship]=$(gum style --foreground "$PINK" "starship")
-	[git]=$(gum style --foreground "$PINK" "git")
+  [bat]=$(gum style --foreground "$PINK" "bat")
+  [eza]=$(gum style --foreground "$PINK" "eza")
+  [nvim]=$(gum style --foreground "$PINK" "nvim")
+  [starship]=$(gum style --foreground "$PINK" "starship")
+  [git]=$(gum style --foreground "$PINK" "git")
+  [zed]=$(gum style --foreground "$PINK" "zed")
 )
-declare -a ALIASES=("bat" "exa" "nvim" "git")
-declare -a GIT_CONFIG=("nvim" "git")
+declare -a ALIASES=("bat" "eza" "nvim" "git")
 
 install_sh_aliases() {
 
-	concatenate_lines_in_file "# sh aliases" "$HOME/.zshrc"
-	concatenate_lines_in_file "[[ -s \"$CURRENT_DIR/aliases/sh/.aliases\" ]] && source \"$CURRENT_DIR/aliases/sh/.aliases\"" "$HOME/.zshrc"
-	echo "Custom aliases installed:"
-	cat aliases/sh/.aliases
+  concatenate_lines_in_file "# sh aliases" "$HOME/.zshrc"
+  concatenate_lines_in_file "[[ -s \"$CURRENT_DIR/aliases/sh/.aliases\" ]] && source \"$CURRENT_DIR/aliases/sh/.aliases\"" "$HOME/.zshrc"
+  echo "Custom aliases installed:"
+  cat aliases/sh/.aliases
 }
 
 install_starship() {
 
-	concatenate_lines_in_file "eval \"$(starship init zsh)\"" "$HOME/.zshrc"
-	cp -rf "starship/starship.toml" "$HOME/.config/starship.toml"
+  concatenate_lines_in_file "eval \"$(starship init zsh)\"" "$HOME/.zshrc"
+  cp -rf "starship/starship.toml" "$HOME/.config/starship.toml"
 }
 
 install_gitconfig() {
 
-	nvim_gitconfig="path = $CURRENT_DIR/git/nvim/.gitconfig"
-	git_gitconfig="path = $CURRENT_DIR/git/.gitconfig"
+  git_gitconfig="path = $CURRENT_DIR/git/.gitconfig"
 
-	for gitConfig in "${GIT_CONFIG[@]}"; do
-		if [ "$app" = "$gitConfig" ]; then
-			if [ "$app" = nvim ]; then
-				if ! grep -qF "$nvim_gitconfig" "$HOME/.gitconfig"; then
-					echo "[include]" >>"$HOME/.gitconfig"
-					echo "$nvim_gitconfig" >>"$HOME/.gitconfig"
-				fi
-			else
-				if ! grep -qF "$git_gitconfig" "$HOME/.gitconfig"; then
-					echo "[include]" >>"$HOME/.gitconfig"
-					echo "$git_gitconfig" >>"$HOME/.gitconfig"
-				fi
-			fi
-		fi
-	done
+  if [ "$app" = git ]; then
+    if ! grep -qF "$git_gitconfig" "$HOME/.gitconfig"; then
+      echo "[include]" >>"$HOME/.gitconfig"
+      echo "$git_gitconfig" >>"$HOME/.gitconfig"
+    fi
+  fi
 }
 
 concatenate_lines_in_file() {
 
-	grep -qF "$1" "$2" || echo "$1" >>"$2"
+  grep -qF "$1" "$2" || echo "$1" >>"$2"
 }
 
 install_aliases() {
 
-	for alias in "${ALIASES[@]}"; do
-		if [ "$app" = "$alias" ]; then
-			concatenate_lines_in_file "# $1 aliases" "$HOME/.zshrc"
-			concatenate_lines_in_file "[[ -s \"$CURRENT_DIR/aliases/$1/.aliases\" ]] && source \"$CURRENT_DIR/aliases/$1/.aliases\"" "$HOME/.zshrc"
+  for alias in "${ALIASES[@]}"; do
+    if [ "$app" = "$alias" ]; then
+      concatenate_lines_in_file "# $1 aliases" "$HOME/.zshrc"
+      concatenate_lines_in_file "[[ -s \"$CURRENT_DIR/aliases/$1/.aliases\" ]] && source \"$CURRENT_DIR/aliases/$1/.aliases\"" "$HOME/.zshrc"
 
-			if [[ $1 = nvim ]]; then
-				install_gitconfig "path = $CURRENT_DIR/git/nvim/.gitconfig"
-			fi
-		fi
-	done
+      if [[ $1 = nvim ]]; then
+        install_gitconfig "path = $CURRENT_DIR/git/nvim/.gitconfig"
+      fi
+    fi
+  done
 }
 
 brew_install_app() {
 
-	if ! command -v "$1" >/dev/null 2>&1; then
+  if ! command -v "$1" >/dev/null 2>&1; then
 
-		echo "${APPS_NAME[$1]} is not installed."
-		echo
-		gum spin -s dot --show-output --title "Installing ${APPS_NAME[$1]} " brew install "$1"
-		echo
-		echo "${APPS_NAME[$1]} installed successfully."
-		echo
+    echo "${APPS_NAME[$1]} is not installed."
+    echo
+    gum spin -s dot --show-output --title "Installing ${APPS_NAME[$1]} " brew install "$1"
+    echo
+    echo "${APPS_NAME[$1]} installed successfully."
+    echo
 
-	else
+  else
 
-		echo "${APPS_NAME[$1]} is currently installed."
-		echo
-	fi
+    echo "${APPS_NAME[$1]} is currently installed."
+    echo
+  fi
 }
 
 install_apps() {
 
-	echo Choose apps you want to install.
-	echo Press space to select between options and press enter to confirm.
-	echo
+  echo Choose apps you want to install.
+  echo Press space to select between options and press enter to confirm.
+  echo
 
-	apps=$(gum choose --no-limit "${APPS_NAME[@]}")
+  apps=$(gum choose --no-limit "${APPS_NAME[@]}")
 
-	# shellcheck disable=SC2068
-	for app in ${apps[@]}; do
-		brew_install_app "$app"
-		if [ "$app" = starship ]; then
-			install_starship
-		else
-			install_aliases "$app"
-			install_gitconfig "$app"
-		fi
-	done
+  # shellcheck disable=SC2068
+  for app in ${apps[@]}; do
+    brew_install_app "$app"
+    if [ "$app" = starship ]; then
+      install_starship
+    else
+      install_aliases "$app"
+      install_gitconfig "$app"
+    fi
+  done
 }
 
 touch "$HOME/.zshrc"
